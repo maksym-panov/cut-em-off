b456eb95af0f7b57d7ece211c23bac037d7660c4
maksym-panov <work.maksym.panov@gmail.com>
2023-10-02 18:27:49 +0300

    Added discovery server service

--- shortener/src/main/java/com/panov/shortener/ShortcutRepository.java
+++
@@ -1,7 +0,0 @@
-package com.panov.shortener;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShortcutRepository extends MongoRepository<Shortcut, String> {}
